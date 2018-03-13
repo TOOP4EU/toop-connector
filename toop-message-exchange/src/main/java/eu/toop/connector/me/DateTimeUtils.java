9176c6505b1e9e28e62c2ab01b4c61b8da5d10a6
Philip Helger <philip@helger.com>
2018-03-13 16:54:41 +0100

    Less warnings

--- toop-message-exchange/src/main/java/eu/toop/connector/me/DateTimeUtils.java
+++ toop-message-exchange/src/main/java/eu/toop/connector/me/DateTimeUtils.java
@@ -19,23 +19,31 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+import javax.annotation.Nonnull;
+
+import com.helger.commons.annotation.Nonempty;
+
 /**
  * Utilities related to date-time formatting, parsing etc..
  *
  * @author: myildiz
  * @date: 16.02.2018.
  */
-public class DateTimeUtils {
+public final class DateTimeUtils {
+  private DateTimeUtils () {}
+
   /**
    * Create the current date time string in format
    *
    * uuuu-MM-dd'T'HH:mm:ss.SSSX
    *
    *
-   * @return
+   * @return current date time with milliseconds and timezone in UTC
    */
+  @Nonnull
+  @Nonempty
   public static String getCurrentTimestamp() {
-    ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
+    final ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
     return now.format(DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSSX"));
   }
 }
