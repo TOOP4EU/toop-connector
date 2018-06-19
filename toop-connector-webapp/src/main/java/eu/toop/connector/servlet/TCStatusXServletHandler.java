06c11dda1b3e1b70c102ead63af60c236bcd3778
Philip Helger <philip@helger.com>
2018-06-19 21:32:46 +0200

    Fixed license headers

--- toop-connector-webapp/src/main/java/eu/toop/connector/servlet/TCStatusXServletHandler.java
+++ toop-connector-webapp/src/main/java/eu/toop/connector/servlet/TCStatusXServletHandler.java
@@ -1,6 +1,5 @@
 /**
- * Copyright (C) 2014-2018 Philip Helger (www.helger.com)
- * philip[at]helger[dot]com
+ * Copyright (C) 2018 toop.eu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
