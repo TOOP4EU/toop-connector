163f7772cc398c9abe615e3f20e2d78f9bc1abb6
Philip Helger <philip@helger.com>
2019-07-16 18:03:18 +0200

    Moved init code to base JAR

--- toop-connector-webapp/src/main/java/eu/toop/connector/servlet/TCWebAppListener.java
+++ toop-connector-webapp/src/main/java/eu/toop/connector/servlet/TCWebAppListener.java
@@ -15,37 +15,13 @@
  */
 package eu.toop.connector.servlet;
 
-import java.net.InetAddress;
-import java.net.URI;
-import java.net.UnknownHostException;
-
 import javax.annotation.Nonnull;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.annotation.WebListener;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-
-import com.helger.commons.debug.GlobalDebug;
-import com.helger.commons.error.level.EErrorLevel;
-import com.helger.commons.exception.InitializationException;
-import com.helger.commons.id.factory.GlobalIDFactory;
-import com.helger.commons.id.factory.StringIDFromGlobalLongIDFactory;
-import com.helger.commons.string.StringHelper;
-import com.helger.commons.url.IURLProtocol;
-import com.helger.commons.url.URLProtocolRegistry;
 import com.helger.web.servlets.scope.WebScopeListener;
 
-import eu.toop.commons.exchange.ToopRequestWithAttachments140;
-import eu.toop.commons.exchange.ToopResponseWithAttachments140;
-import eu.toop.connector.api.TCConfig;
-import eu.toop.connector.api.as4.IMessageExchangeSPI.IIncomingHandler;
-import eu.toop.connector.api.as4.MEException;
-import eu.toop.connector.api.as4.MessageExchangeManager;
-import eu.toop.connector.app.CTC;
-import eu.toop.connector.app.mp.MessageProcessorDCIncoming;
-import eu.toop.connector.app.mp.MessageProcessorDPIncoming;
-import eu.toop.kafkaclient.ToopKafkaClient;
-import eu.toop.kafkaclient.ToopKafkaSettings;
+import eu.toop.connector.app.TCInit;
 
 /**
  * Global startup/shutdown listener for the whole web application. Extends from
@@ -56,116 +32,19 @@ import eu.toop.kafkaclient.ToopKafkaSettings;
 @WebListener
 public class TCWebAppListener extends WebScopeListener
 {
-  private String m_sLogPrefix;
-
   @Override
   public void contextInitialized (@Nonnull final ServletContextEvent aEvent)
   {
     // Must be first
     super.contextInitialized (aEvent);
 
-    GlobalIDFactory.setPersistentStringIDFactory (new StringIDFromGlobalLongIDFactory ("toop-mp-"));
-    GlobalDebug.setDebugModeDirect (TCConfig.isGlobalDebug ());
-    GlobalDebug.setProductionModeDirect (TCConfig.isGlobalProduction ());
-
-    m_sLogPrefix = TCConfig.getToopInstanceName ();
-    if (StringHelper.hasNoText (m_sLogPrefix))
-    {
-      // Get my IP address for debugging as default
-      try
-      {
-        m_sLogPrefix = "[" + InetAddress.getLocalHost ().getHostAddress () + "] ";
-      }
-      catch (final UnknownHostException ex)
-      {
-        m_sLogPrefix = "";
-      }
-    }
-    else
-    {
-      if (!m_sLogPrefix.startsWith ("["))
-        m_sLogPrefix = "[" + m_sLogPrefix + "]";
-
-      // Would have been trimmed when reading the properties file, so add
-      // manually
-      m_sLogPrefix += " ";
-    }
-
-    {
-      // Init tracker client
-      ToopKafkaSettings.setKafkaEnabled (TCConfig.isToopTrackerEnabled ());
-      if (TCConfig.isToopTrackerEnabled ())
-      {
-        final String sToopTrackerUrl = TCConfig.getToopTrackerUrl ();
-        if (StringHelper.hasNoText (sToopTrackerUrl))
-          throw new InitializationException ("If the tracker is enabled, the tracker URL MUST be provided in the configuration file!");
-
-        // Consistency check - no protcol like "http://" or so may be present
-        final IURLProtocol aProtocol = URLProtocolRegistry.getInstance ().getProtocol (sToopTrackerUrl);
-        if (aProtocol != null)
-          throw new InitializationException ("The tracker URL MUST NOT start with a protocol like '" +
-                                             aProtocol.getProtocol () +
-                                             "'!");
-        ToopKafkaSettings.defaultProperties ().put (ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, sToopTrackerUrl);
-
-        final String sToopTrackerTopic = TCConfig.getToopTrackerTopic ();
-        ToopKafkaSettings.setKafkaTopic (sToopTrackerTopic);
-      }
-    }
-
-    {
-      // Check R2D2 configuration
-      final String sDirectoryURL = TCConfig.getR2D2DirectoryBaseUrl ();
-      if (StringHelper.hasNoText (sDirectoryURL))
-        throw new InitializationException ("The URL of the TOOP Directory is missing in the configuration file!");
-
-      if (!TCConfig.isR2D2UseDNS ())
-      {
-        final URI aSMPURI = TCConfig.getR2D2SMPUrl ();
-        if (aSMPURI == null)
-          throw new InitializationException ("Since the usage of SML/DNS is disabled, the fixed URL of the SMP to be used must be provided in the configuration file!");
-      }
-    }
-
-    ToopKafkaClient.send (EErrorLevel.INFO,
-                          () -> m_sLogPrefix + "TOOP Connector WebApp " + CTC.getVersionNumber () + " startup");
-
-    // Init incoming message handler
-    MessageExchangeManager.getConfiguredImplementation ()
-                          .registerIncomingHandler (aEvent.getServletContext (), new IIncomingHandler ()
-                          {
-                            public void handleIncomingRequest (@Nonnull final ToopRequestWithAttachments140 aRequest) throws MEException
-                            {
-                              ToopKafkaClient.send (EErrorLevel.INFO,
-                                                    () -> "TC got DP incoming MEM request (2/4) with " +
-                                                          aRequest.attachments ().size () +
-                                                          " attachments");
-                              MessageProcessorDPIncoming.getInstance ().enqueue (aRequest);
-                            }
-
-                            public void handleIncomingResponse (@Nonnull final ToopResponseWithAttachments140 aResponse) throws MEException
-                            {
-                              ToopKafkaClient.send (EErrorLevel.INFO,
-                                                    () -> "TC got DC incoming MEM request (4/4) with " +
-                                                          aResponse.attachments ().size () +
-                                                          " attachments");
-                              MessageProcessorDCIncoming.getInstance ().enqueue (aResponse);
-                            }
-                          });
-
-    ToopKafkaClient.send (EErrorLevel.INFO, () -> m_sLogPrefix + "TOOP Connector started");
+    TCInit.initGlobally (aEvent.getServletContext ());
   }
 
   @Override
   public void contextDestroyed (@Nonnull final ServletContextEvent aEvent)
   {
-    ToopKafkaClient.send (EErrorLevel.INFO, () -> m_sLogPrefix + "TOOP Connector shutting down");
-
-    // Shutdown message exchange
-    MessageExchangeManager.getConfiguredImplementation ().shutdown (aEvent.getServletContext ());
-
-    // Shutdown tracker
-    ToopKafkaClient.close ();
+    TCInit.shutdownGlobally (aEvent.getServletContext ());
 
     // Must be last
     super.contextDestroyed (aEvent);
---
+++ toop-connector/src/main/java/eu/toop/connector/app/TCInit.java
@@ -0,0 +1,198 @@
+package eu.toop.connector.app;
+
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.UnknownHostException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.NotThreadSafe;
+import javax.servlet.ServletContext;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import com.helger.commons.debug.GlobalDebug;
+import com.helger.commons.error.level.EErrorLevel;
+import com.helger.commons.exception.InitializationException;
+import com.helger.commons.id.factory.GlobalIDFactory;
+import com.helger.commons.id.factory.StringIDFromGlobalLongIDFactory;
+import com.helger.commons.string.StringHelper;
+import com.helger.commons.url.IURLProtocol;
+import com.helger.commons.url.URLProtocolRegistry;
+
+import eu.toop.commons.exchange.ToopRequestWithAttachments140;
+import eu.toop.commons.exchange.ToopResponseWithAttachments140;
+import eu.toop.connector.api.TCConfig;
+import eu.toop.connector.api.as4.IMessageExchangeSPI.IIncomingHandler;
+import eu.toop.connector.api.as4.MEException;
+import eu.toop.connector.api.as4.MessageExchangeManager;
+import eu.toop.connector.app.mp.MessageProcessorDCIncoming;
+import eu.toop.connector.app.mp.MessageProcessorDPIncoming;
+import eu.toop.kafkaclient.ToopKafkaClient;
+import eu.toop.kafkaclient.ToopKafkaSettings;
+
+/**
+ * Contains TOOP Connector global init and shutdown methods.
+ *
+ * @author Philip Helger
+ */
+@NotThreadSafe
+public class TCInit
+{
+  private static final AtomicBoolean INITED = new AtomicBoolean (false);
+  private static String s_sLogPrefix;
+
+  private TCInit ()
+  {}
+
+  /**
+   * Globally init the TOOP Connector. Calling it, if it is already initialized
+   * will thrown an exception.
+   *
+   * @param aServletContext
+   *        The servlet context used for initialization. May not be
+   *        <code>null</code> but maybe a mocked one.
+   * @throws IllegalStateException
+   *         If the TOOP Connector is already initialized
+   * @throws InitializationException
+   *         If any of the settings are totally bogus
+   */
+  public static void initGlobally (@Nonnull final ServletContext aServletContext)
+  {
+    if (!INITED.compareAndSet (false, true))
+      throw new IllegalStateException ("TOOP Connector is already initialized");
+
+    GlobalIDFactory.setPersistentStringIDFactory (new StringIDFromGlobalLongIDFactory ("toop-mp-"));
+    GlobalDebug.setDebugModeDirect (TCConfig.isGlobalDebug ());
+    GlobalDebug.setProductionModeDirect (TCConfig.isGlobalProduction ());
+
+    String sLogPrefix = TCConfig.getToopInstanceName ();
+    if (StringHelper.hasNoText (sLogPrefix))
+    {
+      // Get my IP address for debugging as default
+      try
+      {
+        sLogPrefix = "[" + InetAddress.getLocalHost ().getHostAddress () + "] ";
+      }
+      catch (final UnknownHostException ex)
+      {
+        sLogPrefix = "";
+      }
+    }
+    else
+    {
+      if (!sLogPrefix.startsWith ("["))
+        sLogPrefix = "[" + sLogPrefix + "]";
+
+      // Would have been trimmed when reading the properties file, so add
+      // manually
+      sLogPrefix += " ";
+    }
+    s_sLogPrefix = sLogPrefix;
+
+    {
+      // Init tracker client
+      ToopKafkaSettings.setKafkaEnabled (TCConfig.isToopTrackerEnabled ());
+      if (TCConfig.isToopTrackerEnabled ())
+      {
+        final String sToopTrackerUrl = TCConfig.getToopTrackerUrl ();
+        if (StringHelper.hasNoText (sToopTrackerUrl))
+          throw new InitializationException ("If the tracker is enabled, the tracker URL MUST be provided in the configuration file!");
+
+        // Consistency check - no protcol like "http://" or so may be present
+        final IURLProtocol aProtocol = URLProtocolRegistry.getInstance ().getProtocol (sToopTrackerUrl);
+        if (aProtocol != null)
+          throw new InitializationException ("The tracker URL MUST NOT start with a protocol like '" +
+                                             aProtocol.getProtocol () +
+                                             "'!");
+        ToopKafkaSettings.defaultProperties ().put (ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, sToopTrackerUrl);
+
+        final String sToopTrackerTopic = TCConfig.getToopTrackerTopic ();
+        ToopKafkaSettings.setKafkaTopic (sToopTrackerTopic);
+      }
+    }
+
+    {
+      // Check R2D2 configuration
+      final String sDirectoryURL = TCConfig.getR2D2DirectoryBaseUrl ();
+      if (StringHelper.hasNoText (sDirectoryURL))
+        throw new InitializationException ("The URL of the TOOP Directory is missing in the configuration file!");
+
+      if (!TCConfig.isR2D2UseDNS ())
+      {
+        final URI aSMPURI = TCConfig.getR2D2SMPUrl ();
+        if (aSMPURI == null)
+          throw new InitializationException ("Since the usage of SML/DNS is disabled, the fixed URL of the SMP to be used must be provided in the configuration file!");
+      }
+    }
+
+    ToopKafkaClient.send (EErrorLevel.INFO,
+                          () -> s_sLogPrefix + "TOOP Connector WebApp " + CTC.getVersionNumber () + " startup");
+
+    // Init incoming message handler
+    MessageExchangeManager.getConfiguredImplementation ()
+                          .registerIncomingHandler (aServletContext, new IIncomingHandler ()
+                          {
+                            public void handleIncomingRequest (@Nonnull final ToopRequestWithAttachments140 aRequest) throws MEException
+                            {
+                              ToopKafkaClient.send (EErrorLevel.INFO,
+                                                    () -> s_sLogPrefix +
+                                                          "TC got DP incoming MEM request (2/4) with " +
+                                                          aRequest.attachments ().size () +
+                                                          " attachments");
+                              MessageProcessorDPIncoming.getInstance ().enqueue (aRequest);
+                            }
+
+                            public void handleIncomingResponse (@Nonnull final ToopResponseWithAttachments140 aResponse) throws MEException
+                            {
+                              ToopKafkaClient.send (EErrorLevel.INFO,
+                                                    () -> s_sLogPrefix +
+                                                          "TC got DC incoming MEM request (4/4) with " +
+                                                          aResponse.attachments ().size () +
+                                                          " attachments");
+                              MessageProcessorDCIncoming.getInstance ().enqueue (aResponse);
+                            }
+                          });
+
+    ToopKafkaClient.send (EErrorLevel.INFO, () -> s_sLogPrefix + "TOOP Connector started");
+  }
+
+  /**
+   * @return <code>true</code> if the TOOP Connector was initialized (or is
+   *         currently initializing or is currently shutdown),
+   *         <code>false</code> if not.
+   */
+  public static boolean isInitialized ()
+  {
+    return INITED.get ();
+  }
+
+  /**
+   * Globally shutdown the TOOP Connector. Calling it, if it was not already
+   * initialized will thrown an exception.
+   *
+   * @param aServletContext
+   *        The servlet context used for shutdown. May not be <code>null</code>
+   *        but maybe a mocked one.
+   * @throws IllegalStateException
+   *         If the TOOP Connector is not initialized
+   */
+  public static void shutdownGlobally (@Nonnull final ServletContext aServletContext)
+  {
+    if (!isInitialized ())
+      throw new IllegalStateException ("TOOP Connector is not initialized");
+
+    ToopKafkaClient.send (EErrorLevel.INFO, () -> s_sLogPrefix + "TOOP Connector shutting down");
+
+    // Shutdown message exchange
+    MessageExchangeManager.getConfiguredImplementation ().shutdown (aServletContext);
+
+    // Shutdown tracker
+    ToopKafkaClient.close ();
+
+    s_sLogPrefix = null;
+
+    if (!INITED.compareAndSet (true, false))
+      throw new IllegalStateException ("TOOP Connector is already shutdown");
+  }
+}
